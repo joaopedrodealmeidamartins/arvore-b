163038ea72e85aa16303292c0908cc2877a2e3a3
João Pedro <joaomartins.apk@gmail.com>
2021-11-26 10:13:17 -0300

    Page test

--- pagina.c
+++ pagina.c
@@ -1,34 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
-#include <string.h>
+
+#define ordem 4 
 
 typedef struct {
     int contaChaves;
-    char chaves[3];
-    int filhos[4];
+    int chaves[ordem - 1];
+    int filhos[ordem];
 } Pagina;
 
 
 int main () {
     FILE *arq;
+    int rrn = 2;
 
     if ((arq = fopen("teste.txt", "w")) == NULL) {
         printf("Erro na criacao do arquivo --- programa abortado\n");
         exit(1);
     }
 
-    fseek(arq,1,SEEK_END);
+    fwrite(&rrn, sizeof(int), 1, arq);
+
     Pagina page;
 
-    page.contaChaves = 2;
-    page.chaves[0] = 'I';
-    page.chaves[1] = 'J';
-    page.chaves[2] = 'T';
-    page.filhos[0] = 0;
-    page.filhos[1] = 2;
-    page.filhos[2] = 3;
-    page.filhos[2] = 6;
+    Pagina page0;
+    page0.contaChaves = 2;
+    page0.chaves[0] = 1;
+    page0.chaves[1] = 2;
 
-    fwrite(&page, sizeof(page), 1, arq);
+    fwrite(&page0, sizeof(page), 1, arq);
+
+    Pagina page1;
+    page1.contaChaves = 1;
+    page1.chaves[0] = 4;
+
+    fwrite(&page1, sizeof(page), 1, arq);
+
+    Pagina page2;
+    page2.contaChaves = 1;
+    page2.chaves[0] = 3;
+    page2.filhos[0] = 0;
+    page2.filhos[1] = 1;
+
+    fwrite(&page2, sizeof(page), 1, arq);
 }
