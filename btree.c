669c5da7f64ed7a6e37caa900202c11573011c36
João Pedro <joaomartins.apk@gmail.com>
2021-11-29 00:17:26 -0300

    buscaPagina

--- btree.c
+++ btree.c
@@ -1,72 +1,75 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-//#include "util.h"
+#include <string.h>
 
-#define ORDEM 5
-#define DELIM_STR "|"
-#define TAM_MAX_REG 256
+#define ORDEM 4
 
-void criacao(char *);
-void imprimeInfo();
-void imprimeInfoDec();
-void limpa_buffer(char *, int);
+typedef struct {
+    int conta_chaves;
+    int chave[ORDEM - 1];
+    int filho[ORDEM];
+} Pagina;
 
-int main(int argc, char *argv[])
-{
-    char *nome_arq;
+int busca(FILE *, int);
+int busca_na_pagina(int, Pagina, int);
+Pagina pagina(FILE *, Pagina);
 
-    if (argc == 3 && strcmp(argv[1], "-c") == 0)
-    {
-        printf("Modo de criacao ativado ... nome do arquivo = %s\n", argv[2]);
-        criacao(argv[2]);
+
+int main(int argc, char *argv[]) {
+    FILE *arq;
+
+    if ((arq = fopen("btree.dat", "rb")) == NULL) {
+        printf("Erro na abertura do arquivo --- programa abortado\n");
+        exit(1);
     }
-    /*else if (argc == 2 && strcmp(argv[1], "-p") == 0)
-    {
-        printf("Modo de impressao das informacoes ativado ... );
-        imprimeInfo();
+
+    if (argc == 3 && strcmp(argv[1], "-b") == 0) {
+        busca(arq, atoi(argv[2]));
     }
-    else if (argc == 2 && strcmp(argv[1], "-k") == 0)
-    {
-        printf("Modo de impressao das informacoes ordenadas ativado ...);
-        //imprimeInfoDec();
-    }
-    else
-    {
-        fprintf(stderr, "Argumentos incorretos!\n");
-        fprintf(stderr, "Modo de uso:\n");
-        fprintf(stderr, "$ %s (-i|-e) nome_arquivo\n", argv[0]);
-        fprintf(stderr, "$ %s -p\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    return 0;*/
+
 }
 
-void criacao(char *arq_read)
-{
-    typedef struct pagina
-    {
-        int chave[ORDEM - 1];
-        int filhos[ORDEM];
-
-    } pagina;
+int busca(FILE *arq, int chave) {
+    int pos;
     int rrn;
-    int chave;
-    FILE *arq;
-    FILE *arqDados;
-    char buffer[TAM_MAX_REG];
+    int raiz = 1;
+    int contaChaves;
+    Pagina pag;
 
-    if ((arq = fopen(arq_read, "r")) != NULL)
-    {
-        if ((arqDados = fopen("btree.dat", "wb")) != NULL)
-        {
-            limpa_buffer(buffer, TAM_MAX_REG);
-            //fwrite();
+    int byteOffsetRaiz = (raiz * sizeof(Pagina)) + sizeof(int);
+    fseek(arq, byteOffsetRaiz, SEEK_SET);
+    
+    pagina(arq, pag);
+    if (chave == busca_na_pagina(chave, pag, pos)) {
+        printf("Chave encontrada!!!\n");
+    } else if (chave == -1) { 
+        printf("Chave não encontrada!!!\n");
+    }
+}
+
+int busca_na_pagina(int chave, Pagina pag, int pos) {
+    int i = 0;
+    while (i < pag.conta_chaves && chave > pag.chave[i]) {
+        i++;
+        pos = i;
+        if (pos < pag.conta_chaves && chave == pag.chave[pos]) {
+            return chave;
+        } else {
+            return -1;
         }
     }
-
-    fseek(arq, 0, SEEK_SET);
-    fread(&arqDados, sizeof(int), 1, arq);
-    input(arq, TAM_MAX_REG);
-    limpa_buffer(buffer, TAM_MAX_REG);
 }
+
+Pagina pagina(FILE *arq, Pagina pag) {
+    fread(&pag.conta_chaves, sizeof(int), 1, arq);
+    fread(&pag.chave[0], sizeof(int), 1, arq);
+    fread(&pag.chave[1], sizeof(int), 1, arq);
+    fread(&pag.chave[2], sizeof(int), 1, arq);
+    fread(&pag.filho[0], sizeof(int), 1, arq);
+    fread(&pag.filho[1], sizeof(int), 1, arq);
+    fread(&pag.filho[2], sizeof(int), 1, arq);
+    fread(&pag.filho[3], sizeof(int), 1, arq);
+    fread(&pag.filho[4], sizeof(int), 1, arq);
+}
+//gcc btree.c -o btree
+//.\btree.exe -b 2
--- pagina.c
+++ pagina.c
@@ -12,7 +12,6 @@ typedef struct {
 
 int main () {
     FILE *arq;
-    Pagina page;
 
     if ((arq = fopen("btree.dat", "wb")) == NULL) {
         printf("Erro na criacao do arquivo --- programa abortado\n");
@@ -27,38 +26,40 @@ int main () {
     page0.contaChaves = 2;
     page0.chaves[0] = 1;
     page0.chaves[1] = 2;
+    page0.chaves[2] = -1;
     page0.filhos[0] = -1;
     page0.filhos[1] = -1;
     page0.filhos[2] = -1;
-    page0.filhos[2] = -1;
+    page0.filhos[3] = -1;
 
-    fwrite(&page0, sizeof(page), 1, arq);
+    fwrite(&page0, sizeof(Pagina), 1, arq);
 
     Pagina page1;
 
     page1.contaChaves = 1;
     page1.chaves[0] = 4;
+    page1.chaves[1] = -1;
+    page1.chaves[2] = -1;
     page1.filhos[0] = -1;
     page1.filhos[1] = -1;
     page1.filhos[2] = -1;
-    page1.filhos[2] = -1;
+    page1.filhos[3] = -1;
 
-    fwrite(&page1, sizeof(page), 1, arq);
+    fwrite(&page1, sizeof(Pagina), 1, arq);
 
     Pagina page2;
 
     page2.contaChaves = 1;
     page2.chaves[0] = 3;
+    page2.chaves[1] = -1;
+    page2.chaves[2] = -1;
     page2.filhos[0] = 0;
     page2.filhos[1] = 1;
     page2.filhos[2] = -1;
-    page2.filhos[2] = -1;
+    page2.filhos[3] = -1;
 
-    fwrite(&page2, sizeof(page), 1, arq);
+    fwrite(&page2, sizeof(Pagina), 1, arq);
 }
 
 // gcc pagina.c -o createBTree
 // .\createBTree.exe
-
-// byteOffset = (raiz*sizeOf(pagina)) + sizeOf(int)
-//2212-1-1-1-1-114-1-1-1-1-1-113-1-101-1-1
